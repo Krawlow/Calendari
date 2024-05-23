d4db01732f2442b1273ddfd7f7ffcad133cb8389
Krawlow <mglarecerca@gmail.com>
2024-05-23 22:00:33 +0200

    created with the help of chat gpt a function to cycle the states and returned the full calendar, adding also a post processing bit in the main that calculates the duration of each state and prints it in a visible manner

--- src/create_calendar.py
+++ src/create_calendar.py
@@ -3,34 +3,41 @@ from itertools import cycle
 import random
 import numpy
 
-def setup_cycle(states, initial_state):
-    initial_state_of_cycle = states.index(initial_state)
-    states_cycle = cycle(states)
-    for i in range(initial_state_of_cycle):
-        states_cycle.next()
-    return states_cycle
+class CyclingStates:
+    def __init__(self, states, initial):
+        if initial not in states:
+            raise ValueError("Initial state must be in the list of states")
+        self.states = states
+        self.index = self.states.index(initial)
+    
+    def next(self):
+        self.index = (self.index +1) % len(self.states)
+        return self.states[self.index]
+    
+    def current_state(self):
+        return self.states[self.index]
 
 def time_to_change(swap_prob):
     return random.random() < swap_prob
 
-def create_calendar(states, initial_state, swap_prob, days, months, years):
+def create_calendar(states, initial_state, swap_prob, cycles, weeks, months):
     # set up the initial state of the cycle
-    states_cycle = setup_cycle(states,initial_state)
+    states_cycle = CyclingStates(states,initial_state)
 
-    state_changes = numpy.zeros(shape=(years,months,days))
+    state_changes = numpy.zeros(shape=(months,weeks,cycles))
+    calendar = []
 
-    for year in range(years):
-        for month in range(months):
-            for day in range(days):
-                state_changes[year][month][day] = time_to_change(swap_prob)
-                if (state_changes[year][month][day]):
-                    print(f"Time to change! On year {year}, month {month} and day {day}")
-
-
-    return state_changes
-
-
-    
+    for month in range(months):
+        for week in range(weeks):
+            for cycle in range(cycles):
+                state_changes[month][week][cycle] = time_to_change(swap_prob)
+                if (state_changes[month][week][cycle]):
+                    calendar.append(states_cycle.next())
+                    if (False):
+                        print(f"Time to change! On month {month}, week {week} and cycle {cycle}")
+                else:
+                    calendar.append(states_cycle.current_state())
+    return calendar
 
 if __name__ == "__main__":
     # load calendar configuration from file and test if it is a good configuration
@@ -40,8 +47,16 @@ if __name__ == "__main__":
         calendar_configuration["states"],
         calendar_configuration["initial_state"],
         eval(calendar_configuration["state_change_probability"]),
-        calendar_configuration["days_per_month"],
-        calendar_configuration["months_per_year"],
-        calendar_configuration["years"]
+        calendar_configuration["cycles_per_week"],
+        calendar_configuration["weeks_per_month"],
+        calendar_configuration["months"]
         )
+    current_state = calendar_configuration["initial_state"]
+    length_of_period = 0
+    for cycle in calendar:
+        length_of_period += 1
+        if current_state != cycle:
+            print(f"State Changed! This was the length of the {current_state} period {int(length_of_period/calendar_configuration["cycles_per_week"]//calendar_configuration["weeks_per_month"])} months {length_of_period//calendar_configuration["cycles_per_week"]} weeks and {length_of_period%calendar_configuration["cycles_per_week"]} cycles")
+            current_state = cycle
+            length_of_period = 0
 
